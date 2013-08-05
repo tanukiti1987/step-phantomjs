30c0dd47993c33e3745c7eb1505a4bbee4a7c073
Alan Harper <alan@aussiegeek.net>
2013-08-05 14:23:13 +1000

    Untar everything

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 cd /tmp
 wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
+tar xfj phantomjs-1.9.1-linux-x86_64.tar.bz2
 sudo cp /tmp/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin
