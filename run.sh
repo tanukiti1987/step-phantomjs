0f9d202dfe2bb33f91bb63a40e15f7b4df832e3e
Alan Harper <alan@aussiegeek.net>
2013-08-05 14:00:16 +1000

    Give full path to source

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cd /tmp
 wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
-sudo cp phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin
+sudo cp /tmp/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin
