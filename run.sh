78b7c1db08be55224c074c174fbc51beaf2036ba
Ryusuke Sekiguchi <ryusuke-sekiguchi@cookpad.com>
2016-07-01 11:16:40 +0900

    Version up phantomjs to 2.1.1

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2
-tar xfj phantomjs-1.9.1-linux-x86_64.tar.bz2
-sudo cp /tmp/phantomjs-1.9.1-linux-x86_64/bin/phantomjs /usr/local/bin
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar xfj phantomjs-2.1.1-linux-x86_64.tar.bz2
+sudo cp /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
