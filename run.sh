263547384161594a8a2b13a456f92c64cd953159
Ryusuke Sekiguchi <ryusuke-sekiguchi@cookpad.com>
2017-01-05 14:50:00 +0900

    Fix wget option

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+wget --max-redirect=0 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 tar xfj phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo cp /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
